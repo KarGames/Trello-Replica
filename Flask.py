ac0311bdf91f03f55ef241f9f3f7998d6870c5c2
KarGames <97896384+KarGames@users.noreply.github.com>
2023-05-20 21:13:49 -0400

    allows user to add a task with a date

--- Flask.py
+++ Flask.py
@@ -7,26 +7,23 @@ date = ""
 
 taskCards = []
 
+class TaskCard:
+    def __init__(self, task, date):
+        self.task = task
+        self.date = date
+
 @app.route("/trello", methods=["GET", "POST"])
 def trello():
     if request.method == "POST":
         task = request.form.get("task")
         date = request.form.get("date")
         
-        return render_template("trello.html", task = task, date = date)
-        
+        taskCard = TaskCard(task, date)
+        taskCards.append(taskCard)
+
+        return render_template("trello.html", taskCards = taskCards)
         
     return render_template("trello.html")
-    
-
-
-class taskCard:
-    def __init__(self, task, date):
-        self.task = task
-        self.date = date
-
-print(task)
-print(date)
 
 if __name__ == "__main__":
     app.run(debug=True)
